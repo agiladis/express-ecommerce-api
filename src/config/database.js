46c8e6902479cae0a53b2bb848932cfdb97bac35
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-09-26 02:31:14 +0700

    [UPDATE] user entity, node script

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
-module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: 'express_ecommerce_db',
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
-  },
-};
+    define: {
+      underscored: true,
+    },
+  }
+);
+
+module.exports = sequelize;
--- src/entities/user.js
+++ src/entities/user.js
@@ -1,11 +1,8 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
-const User = sequelize.define('User', {
-  fullName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
+const User = sequelize.define('users', {
+  fullName: DataTypes.STRING,
   email: {
     type: DataTypes.STRING,
     allowNull: false,
