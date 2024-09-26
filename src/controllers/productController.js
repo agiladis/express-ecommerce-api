c7fc7e03003d2c0ab837bd1a8a3b62ea861935ed
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-09-26 23:28:42 +0700

    [UPDATE] filtering, sorting, search from query when find all products

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,14 +1,40 @@
 const { Op } = require('sequelize');
 const Product = require('../entities/product');
+const Category = require('../entities/category');
 
 const getAll = async (req, res) => {
   try {
+    const {
+      page = 1,
+      size = 5,
+      sort = 'createdAt',
+      order = 'DESC',
+      search = '',
+      category = '',
+    } = req.query;
+    const limit = parseInt(size);
+    const offset = (page - 1) * limit;
+
+    const where = { stock: { [Op.gt]: 0 } };
+    if (search) {
+      where.name = { [Op.like]: `%${search}%` };
+    }
+
+    const include = [];
+    if (category) {
+      const categoryInstance = await Category.findOne({
+        where: { name: category },
+      });
+      if (categoryInstance) where.categoryId = categoryInstance.id;
+      //   include.push({ model: Category, where: { id: categoryInstance.id } });
+    }
+
     const products = await Product.findAll({
-      where: {
-        stock: {
-          [Op.gt]: 0,
-        },
-      },
+      where,
+      include,
+      order: [[sort, order.toUpperCase()]],
+      offset,
+      limit,
       attributes: ['name', 'price'],
     });
 
