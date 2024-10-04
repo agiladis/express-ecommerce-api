1395a906584b4a68871b0261390a590bb2a0d5ae
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-10-04 16:54:17 +0700

    [ADD] new entities for cartItem and review. also ADD new associations file

--- src/entities/associations.js
+++ src/entities/associations.js
@@ -1,11 +1,46 @@
+const User = require('./user');
+const Cart = require('./cart');
+const CartItem = require('./cartItems');
+const Category = require('./category');
 const Order = require('./order');
 const OrderItem = require('./orderItem');
 const Product = require('./product');
+const Review = require('./review');
+
+User.hasMany(Order, { foreignKey: 'userId' });
+Order.belongsTo(User, { foreignKey: 'userId' });
 
 Order.hasMany(OrderItem, { foreignKey: 'orderId' });
 OrderItem.belongsTo(Order, { foreignKey: 'orderId' });
 
+User.hasOne(Cart, { foreignKey: 'userId' });
+Cart.belongsTo(User, { foreignKey: 'userId' });
+
+Cart.hasMany(CartItem, { foreignKey: 'cartId' });
+CartItem.belongsTo(Cart, { foreignKey: 'cartId' });
+
+User.hasMany(Review, { foreignKey: 'userId' });
+Review.belongsTo(User, { foreignKey: 'userId' });
+
+Product.belongsTo(Category, { foreignKey: 'categoryId' });
+Category.hasMany(Product, { foreignKey: 'categoryId' });
+
+Product.hasMany(Review, { foreignKey: 'productId' });
+Review.belongsTo(Product, { foreignKey: 'productId' });
+
+CartItem.belongsTo(Product, { foreignKey: 'productId' });
+Product.hasMany(CartItem, { foreignKey: 'productId' });
+
 OrderItem.belongsTo(Product, { foreignKey: 'productId' });
 Product.hasMany(OrderItem, { foreignKey: 'productId' });
 
-module.exports = { Order, OrderItem };
+module.exports = {
+  User,
+  Order,
+  Cart,
+  Product,
+  CartItem,
+  OrderItem,
+  Category,
+  Review,
+};
