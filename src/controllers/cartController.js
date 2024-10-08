1e254036d5ab6210748ed71e967fed091a756784
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-10-08 16:04:51 +0700

    [UPDATE] logic to handle add product to cart with transaction

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,28 +1,49 @@
-const { where } = require('sequelize');
+const sequelize = require('../config/database');
 const Cart = require('../entities/cart');
 const Product = require('../entities/product');
+const CartItem = require('../entities/cartItem');
 
 const addToCart = async (req, res) => {
-  const { productId, quantity = 1 } = req.body;
+  const { productId } = req.body;
   const userId = req.user.id;
 
-  try {
-    const product = await Product.findOne({
-      where: { id: productId },
-    });
-    if (!product) return res.error(404, 'product not found');
-    if (product.stock < quantity) return res.error(400, 'Insufficient stock');
+  const transaction = await sequelize.transaction();
 
-    let cart = await Cart.findOne({ where: { userId, productId } });
-    if (cart) {
-      cart.quantity += quantity;
-      await cart.save();
-    } else {
-      cart = await Cart.create({ userId, productId, quantity });
+  try {
+    const product = await Product.findByPk(productId, { transaction });
+    if (!product) return res.error(404, 'product not found');
+    if (!product.isAvail) return res.error(400, 'product is not available');
+
+    let cart = await Cart.findOne({ where: { userId }, transaction });
+    if (!cart) {
+      cart = await Cart.create({ userId }, { transaction });
     }
 
-    res.success(200, cart, 'Product successfully added to cart');
+    let cartItem = await CartItem.findOne({
+      where: { cartId: cart.id, productId },
+      transaction,
+    });
+    if (cartItem) {
+      cartItem.quantity += 1;
+      await cartItem.save({ transaction });
+    } else {
+      cartItem = await CartItem.create(
+        {
+          cartId: cart.id,
+          productId,
+          quantity: 1,
+        },
+        { transaction }
+      );
+
+      cart.isEmpty = false;
+      await cart.save({ transaction });
+    }
+
+    await transaction.commit();
+    res.success(200, cartItem, 'Product successfully added to cart');
   } catch (error) {
+    await transaction.rollback();
     res.error(500, error.message, 'internal server error');
   }
 };
